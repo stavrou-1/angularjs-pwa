877f63ee9b68790ed079bfa086542f85b6358e81
Yiannis <yiannis@yiannis2.local>
2019-02-23 14:49:48 -0600

    another

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/'));
 
-app.get('/#', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
