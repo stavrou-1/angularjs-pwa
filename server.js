eaad0de7b9f42d080236af0e36ee92582024c25a
Yiannis <yiannis@yiannis2.lan>
2019-03-11 16:00:07 -0500

    Trust proxis

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 var express = require('express'),
-       secure = require('ssl-express-www'),
-       app = express();
+       app = express(),
+       bodyParser = require('body-parser'),
+       port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.enable('trust proxy'); // needed if you're behind a load balancer
+// app.use(express.static(__dirname));
 app.get('/', function(req, res) {
     res.sendfile('index.html', {root: __dirname })
 });
 
-app.use(function(req, res, next) {
-    var sslUrl;
-    if (process.env.NODE_ENV === "production" && req.headers['x-forwarded-proto'] !== 'https') {
-        sslUrl = ['https://angularjs-pwa.herokuapp.com', req.url].join('');
-        return res.redirect(sslUrl);
+// app.use(function(req, res, next) {
+//     if (req.secure) {
+//         return next();
+//     }
+//     res.redirect('https://' + req.headers.host + req.url);
+// });
+
+app.use(function(req, res, next){
+    if(req.header('x-forwarded-proto') !== 'https'){
+        res.redirect('https://' + req.header('host') + req.url);
+    }else{
+        next();
     }
-    return next();
 })
 
-var server = app.listen(process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname))
+
+app.listen(port);
+console.log('App listening on port ' + port);
+
+// var server = app.listen(process.env.PORT || 3000);
+// app.listen(server, function() {
+//     console.log("Node server listening on port");
+// });
