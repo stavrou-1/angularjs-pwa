57abf9bc5d67832889d3a3ad1b8a4e70accdc7ee
Yiannis <yiannis@yiannis2.lan>
2019-03-11 15:13:36 -0500

    Update http redirect

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express'),
        secure = require('ssl-express-www'),
-       app = express();
+       app = express(),
+       http = express.createServer();
 
 app.use(express.static(__dirname));
 app.use(secure);
 app.get('/', function(req, res) {
     res.sendfile('index.html', {root: __dirname })
 });
+
+http.get('*', function(req, res) {
+    if (!req.secure() && req.protocol === 'http') {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 var server = app.listen(process.env.PORT || 3000);
 app.listen(server, function() {
-    console.log("Node server listening on port " + server);
+    console.log("Node server listening on port");
 })
