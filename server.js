531d3a1d38e0065b2a51827473b4975146d144cc
Yiannis <yiannis@yiannis2.lan>
2019-03-11 15:07:57 -0500

    Node ssl-express-www

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express'),
-app = express();
+       secure = require('ssl-express-www'),
+       app = express();
 
 app.use(express.static(__dirname));
+app.use(secure);
 app.get('/', function(req, res) {
     res.sendfile('index.html', {root: __dirname })
 });
-var server = app.listen(process.env.PORT || 80);
+var server = app.listen(process.env.PORT || 3000);
+app.listen(server, function() {
+    console.log("Node server listening on port " + server);
+})
